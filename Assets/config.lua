cead1955b19e2f1dccf023cc621cc419a0fbd592
Blip <amankoushal1985@gmail.com>
2024-10-10 22:09:06 +0530

    Updated config.lua via Discord

--- Assets/config.lua
+++ Assets/config.lua
@@ -1,9 +1,8 @@
-
-        return {
-            Version = 11,
-            GUIVersion = 6.5,
-            Author = "._m4n",
-            IsEnabled = true,
-            IsIdEnabled = true
-        }
-      
+return {
+    "Version": 11,
+    "GUIVersion": 6.5,
+    "Author": "._m4n",
+    "IsEnabled": false,
+    "IsIdEnabled": true,
+    "sha": "f207df4768c02e965299d03d9ed93c3d67acb131"
+}
