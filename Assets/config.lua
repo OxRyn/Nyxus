a501bcf10c061e7cab7033d444058f14cd24e14e
Blip <theamankoushal@gmail.com>
2024-10-10 23:11:17 +0530

    Update config.lua

--- Assets/config.lua
+++ Assets/config.lua
@@ -3,6 +3,5 @@ return {
     "GUIVersion": 6.5,
     "Author": "._m4n",
     "IsEnabled": false,
-    "IsIdEnabled": true,
-    "sha": "f207df4768c02e965299d03d9ed93c3d67acb131"
+    "IsIdEnabled": true
 }
