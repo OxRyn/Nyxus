0fe459b23bc52d5f70717757ce6daf7ca7053b1e
Blip <amankoushal1985@gmail.com>
2024-10-10 17:17:19 +0530

    Updated IsEnabled to false

--- Assets/config.lua
+++ Assets/config.lua
@@ -1,9 +1,9 @@
-return {
-    Version = 12,
-    GUIVersion = 6.5,
-    Author = "._m4n",
-    Loadstring = (loadstring(game:HttpGet("https://raw.githubusercontent.com/ItzAmanKoushal/Nyxus/main/Elevate.main.lua")))(),
-    IsEnabled = true,
-    IsIdEnabled = true,
-    IsOwner = false
-}
+
+        return {
+            Version = 11,
+            GUIVersion = 6.5,
+            Author = "._m4n",
+            IsEnabled = false,
+            IsIdEnabled = true
+        }
+      
