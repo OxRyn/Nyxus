f045b4fbc5e25013ffadbf293cfa1834b80133d9
Blip <amankoushal1985@gmail.com>
2024-10-11 13:03:19 +0530

    Updated config.lua via patrixx

--- Assets/config.lua
+++ Assets/config.lua
@@ -1,11 +1,11 @@
- return {
-        Author = "._m4n",
-        Package = "mini-patrixx",
-        Version = 12,
-        GUIVersion = 6.5,
-        IsOwner = false,
-        IsEnabled = true,
-        IsIdEnabled = true,
-        Loadstring = "(loadstring(game:HttpGet(\"https://raw.githubusercontent.com/ItzAmanKoushal/Nyxus/main/Elevate.main.lua\")))();",
-        UUID = "a49e41e2-a718-4282-ba98-727a973c8d80"
-    }
+return {
+    "Author": "._m4n",
+    "Package": "mini-patrixx",
+    "Version": 12,
+    "GUIVersion": 6.5,
+    "IsOwner": false,
+    "IsEnabled": false,
+    "IsIdEnabled": true,
+    "Loadstring": "(loadstring(game:HttpGet(\"https://raw.githubusercontent.com/ItzAmanKoushal/Nyxus/main/Elevate.main.lua\")))();",
+    "UUID": "a49e41e2-a718-4282-ba98-727a973c8d80"
+}
