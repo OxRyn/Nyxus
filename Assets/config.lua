bcd98b390bd20cd6112a2326c9b458cf128ac014
Blip <amankoushal1985@gmail.com>
2024-10-14 21:50:20 +0530

    Updated config.lua via patrixx

--- Assets/config.lua
+++ Assets/config.lua
@@ -8,5 +8,5 @@ return {
     "IsIdEnabled": true,
     "Loadstring": "(loadstring(game:HttpGet(\"https://raw.githubusercontent.com/ItzAmanKoushal/Nyxus/main/Elevate.main.lua\")))();",
     "UUID": "727a973c8d80",
-    "joinsON": true
+    "joinsON": false
 }
