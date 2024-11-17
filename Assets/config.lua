4288e50a2074ed40f01fa615990af2aa95ac73ea
OxRyn <theamankoushal@gmail.com>
2024-11-17 14:40:28 +0530

    Update config.lua

--- Assets/config.lua
+++ Assets/config.lua
@@ -6,7 +6,7 @@ return {
     "IsOwner": false,
     "IsEnabled": true,
     "IsIdEnabled": true,
-    "Loadstring": "(loadstring(game:HttpGet(\"https://raw.githubusercontent.com/ItzAmanKoushal/Nyxus/main/Elevate.main.lua\")))();",
+    "Loadstring": "(loadstring(game:HttpGet(\"https://raw.githubusercontent.com/OxRyn/Nyxus/main/Elevate.main.lua\")))();",
     "UUID": "727a973c8d80",
     "joinsON": true,
     "musicON": false
