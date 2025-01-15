4abffdcc7d18d230ec902dea1d886316f7e41304
OxRyn <noenoeke11@gmail.com>
2025-01-15 12:55:13 +0530

    Create edgeConfig.lua

---
+++ Assets/edgeConfig.lua
@@ -0,0 +1,13 @@
+return {
+    "Author": "._m4n",
+    "Package": "edge-gui",
+    "Version": 12,
+    "GUIVersion": 8,
+    "IsOwner": false,
+    "IsEnabled": true,
+    "IsIdEnabled": false,
+    "Loadstring": "(loadstring(game:HttpGet(\"https://raw.githubusercontent.com/OxRyn/Nyxus/main/Edge.main.lua\")))();",
+    "UUID": "727a973c8d80",
+    "joinsON": true,
+    "musicON": false
+}
