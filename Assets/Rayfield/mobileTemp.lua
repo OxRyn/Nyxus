438693c80e75bd9da0473f33ee1baf19b864926d
OxRyn <noenoeke11@gmail.com>
2025-03-26 19:06:00 +0530

    Update mobileTemp.lua

--- Assets/Rayfield/mobileTemp.lua
+++ Assets/Rayfield/mobileTemp.lua
@@ -1,4 +1,18 @@
 
+
+local Script = {
+    Author = "._m4n",
+    Package = "elevate-gui",
+    Version = 12,
+    GUIVersion = 8,
+    IsOwner = false,
+    IsEnabled = true,
+    IsIdEnabled = true,
+    Loadstring = '(loadstring(game:HttpGet("https://raw.githubusercontent.com/OxRyn/Nyxus/main/Elevate.main.lua")))();',
+    UUID = "727a973c8d80",
+    joinsON = true,
+    musicON = false
+}
 local cloneref = cloneref or function(o)
 	return o
 end
