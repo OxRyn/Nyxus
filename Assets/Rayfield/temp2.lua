f7aa5298623a9a230cc8c48f0a0bf9b8b3a30a7d
OxRyn <noenoeke11@gmail.com>
2025-03-26 18:52:54 +0530

    Update temp2.lua

--- Assets/Rayfield/temp2.lua
+++ Assets/Rayfield/temp2.lua
@@ -1,5 +1,5 @@
 local success, result = pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/OxRyn/Nyxus/refs/heads/main/Assets/Rayfield/mobileTemp.min.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/OxRyn/Nyxus/refs/heads/main/Assets/Rayfield/mobileTemp.lua"))()
 end)
 
 if not success then
