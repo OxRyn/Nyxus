0c4a3d54f9dfce2126206c90f80eaf4530e724bb
OxRyn <noenoeke11@gmail.com>
2025-04-01 11:57:53 +0530

    Update ScriptConfigs.lua

--- Assets/ScriptConfigs.lua
+++ Assets/ScriptConfigs.lua
@@ -1,1 +1,13 @@
 
+return {
+    "Author": "._m4n",
+    "Package": "nyxus-raw",
+    "Version": 16,
+    "GUIVersion": 12,
+    "IsOwner": false,
+    "UUID": "8bf9152000e8",
+    "IsEdgeEnabled": true,  
+    "IsGhostEnabled": false, 
+    "IsElevateEnabled": true, 
+    "IsSaranEnabled": false, 
+}
