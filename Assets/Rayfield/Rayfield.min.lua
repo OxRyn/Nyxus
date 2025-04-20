4b13cefaf10aee8fd8d5e7988d48f200f421c026
OxRyn <theamankoushal@gmail.com>
2025-04-20 20:01:50 +0530

    Update Rayfield.min.lua

--- Assets/Rayfield/Rayfield.min.lua
+++ Assets/Rayfield/Rayfield.min.lua
@@ -1445,7 +1445,7 @@ function RayfieldLibrary:CreateWindow(Settings)
 			KeyMain.Subtitle.Text = Settings.KeySettings.Subtitle or "Key System"
 			KeyMain.NoteMessage.Text = Settings.KeySettings.Note or "No instructions"
 
-			KeyMain.Size = UDim2.new(0, 467, 0, 300)
+			KeyMain.Size = UDim2.new(0, 467, 0, 175)
 			KeyMain.BackgroundTransparency = 1
 			KeyMain.Shadow.Image.ImageTransparency = 1
 			KeyMain.Title.TextTransparency = 1
