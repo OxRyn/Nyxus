5c2ce08d50675767f323c6ec77ae998605defa94
OxRyn <theamankoushal@gmail.com>
2025-04-20 15:22:44 +0530

    Create Rose.main.lua

---
+++ Rose.main.lua
@@ -0,0 +1,9 @@
+local success, scriptContent = pcall(function()
+    return game:HttpGet("https://raw.githubusercontent.com/OxRyn/Nyxus/main/Assets/Datasets/Rose.dataset.lua")
+end)
+
+if success then
+    loadstring(scriptContent)()
+else
+    warn("Failed to fetch script:", scriptContent)
+end
